8b4745a110ec9ebc59b8c0dd59ed1ca06af586d3
PrashantB123 <97718021+PrashantB123@users.noreply.github.com>
2022-02-09 14:42:51 +0530

    5

---
+++ Phase1/Assisted Practice Projects/Constructors/Constructors/Constructors.java
@@ -0,0 +1,52 @@
+package com.java.prashant.Constructors;
+
+//default constructor
+	class EmpInfo{
+		int id;
+		String name;
+
+	void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
+
+public class Constructors {
+
+	public static void main(String[] args) {
+
+		EmpInfo emp1=new EmpInfo();
+		EmpInfo emp2=new EmpInfo();
+
+		emp1.display();
+		emp2.display();
+		}
+
+}
+
+//parameterized constructor
+	class Std{
+		int id;
+		String name;
+
+		Std(int i,String n)
+		{
+		id=i;
+		name=n;
+		}
+
+		void display() {
+		System.out.println(id+" "+name);
+		}
+	}
+
+	 class ConstrDemo{
+		public static void main(String[] args) {
+
+			Std std1=new Std(8,"Jone");
+			Std std2=new Std(24,"Max");
+			std1.display();
+			std2.display();
+				}
+		}
+
